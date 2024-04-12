5aafd8d648b2259ee46525c6e9003d755b8798c9
agoy <128023299+ObamaTuru@users.noreply.github.com>
2024-04-13 00:43:44 +0700

    Update flight.sh

--- flight.sh
+++ flight.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#host ganti hostmu
+
+# Daftar host yang ingin Anda cek
 hosts=(
     'http://104.18.49.163/'
     'http://104.18.49.163/'
@@ -10,17 +11,17 @@ while true; do
     success_count=0
 
     for host in "${hosts[@]}"; do
-        if curl "$host" --connect-timeout 5 >/dev/null 2>&1; >
+        if curl "$host" --connect-timeout 5 >/dev/null 2>&1; then
             ((success_count++))
         else
-            echo "[$host] Failed to connect"
+            echo "[$host] Gagal terhubung"
         fi
     done
 
     if [ $success_count -eq 0 ]; then
         sudo cmd connectivity airplane-mode enable
         sudo cmd connectivity airplane-mode disable
-        echo "[System] Enable flight mode"
+        echo "[Sistem] Mengaktifkan mode pesawat"
         sleep 6
     fi
 done
